2c1ff3139943939bed97e6ca2f57684753d92866
Grant Hall <grantclone@gmail.com>
2023-02-28 13:01:04 +0900

    generated all models policys

---
+++ app/policies/preference_policy.rb
@@ -0,0 +1,24 @@
+class PreferencePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+
+    def show?
+      true
+    end
+
+    def create?
+      true
+    end
+
+    def update?
+      true
+    end
+
+    def destroy?
+      true
+    end
+  end
+end
