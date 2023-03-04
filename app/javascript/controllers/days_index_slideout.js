a2412b9c42ebf3c2c896485450e45329377e06e8
Grant Hall <grantclone@gmail.com>
2023-03-05 00:25:01 +0900

    changed calendar position from slideout + changed preference category names

--- app/javascript/controllers/days_index_slideout.js
+++ app/javascript/controllers/days_index_slideout.js
@@ -1,7 +1,8 @@
 function show() {
   console.log("click");
   document.getElementById('sidebar').classList.toggle('active');
-  // document.getElementById('sidebar').classList.toggle('active');
+  document.getElementById('manager-calendar').classList.toggle('justify-content-center');
+  document.getElementById('manager-calendar').classList.toggle('justify-content-end');
 }
 
 document.getElementById('toggle').addEventListener("click", show)
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -2,7 +2,7 @@ class Preference < ApplicationRecord
   belongs_to :user
   belongs_to :day
 
-  enum category: %i[Day-off Paid-Dayoff Time-off]
+  enum category: %i[dayoff vacation shift]
 
   validates :category, presence: true
 end
