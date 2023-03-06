827b6062df9afdb540413b36486453dc2d7f5f8f
Grant Hall <grantclone@gmail.com>
2023-03-06 23:02:21 +0900

    added shift errors to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_072842) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_105312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_072842) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "manager", default: false
+    t.integer "unavailable_day_ids", default: [], array: true
+    t.text "shift_errors", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
