ef4b22ceb7200a529e18d128ec58081c36cc97d1
takaakinakagami <takaaki0120n@gmail.com>
2023-03-06 20:51:11 +0900

    adding mergin on the left

---
+++ app/javascript/controllers/tooltip_controller.js
@@ -0,0 +1,9 @@
+// app/javascript/controllers/tooltip_controller.js
+import { Controller } from "@hotwired/stimulus";
+// import { Tooltip } from "bootstrap/dist/js/bootstrap.bundle.js";
+
+export default class extends Controller {
+  connect() {
+    // new Tooltip(this.element)
+  }
+}
