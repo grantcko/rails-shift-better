ba6baef00187949cfe39a7dd515915f6e7c8da55
takaakinakagami <takaaki0120n@gmail.com>
2023-03-09 20:17:28 +0900

    disappearing the memo

--- app/javascript/controllers/loading_spin_controller.js
+++ app/javascript/controllers/loading_spin_controller.js
@@ -2,9 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading-spin"
 export default class extends Controller {
+  static targets = ["cancel","load"]
   connect() {
+    console.log("loading spin")
   }
-  insertspin(event) {
-    event.currentTarget.innerHTML=" <i class='fa-solid fa-cog fa-spin'></i>"
+  insertspin() {
+    // console.log("maaa")
+    this.loadTarget.innerHTML=" <i class='fa-solid fa-cog fa-spin'></i>"
+    // window.addEventListener('load',()=> {
+    //   console.log("alfjlasdjf");
+    //   this.cancelTarget.style.display= "none";
+    // })
+    // this.cancelTarget.style.display= "none";
+
+
   }
 }
